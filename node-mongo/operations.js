63864722c3cf94b848b72d9e0db39aaf62f542be
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2021-01-14 13:37:42 -0600

    Mongoose ODM Part 1

--- node-mongo/operations.js
+++ node-mongo/operations.js
@@ -1,5 +1,3 @@
-const assert = require('assert');
-
 exports.insertDocument = (db, document, collection, callback) => {
     const coll = db.collection(collection);
     return coll.insert(document);
