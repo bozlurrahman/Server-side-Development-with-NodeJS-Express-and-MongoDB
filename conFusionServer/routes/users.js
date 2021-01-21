daa48759f63210df68c20c915a7d30c40a8247bd
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2021-01-21 17:29:36 -0600

    Passport and JSON Web Token

--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 const bodyParse = require('body-parser');
 var User = require('../models/user');
 var passport = require('passport');
+var authenticate = require('../authenticate');
 
 var router = express.Router();
 router.use(bodyParse.json());
@@ -10,6 +11,7 @@ router.use(bodyParse.json());
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
 
 router.post('/signup', (req, res, next) => {
   User.register(new User({username: req.body.username}), req.body.password, (err, user) => {
@@ -28,11 +30,14 @@ router.post('/signup', (req, res, next) => {
   });
 });
 
+
 router.post('/login', passport.authenticate('local'), (req, res) => {
+  var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, status: 'You are successfully logged in!'});
+  res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
+
 
 router.get('/logout', (req, res, next) => {
   if (req.session) {
@@ -45,6 +50,6 @@ router.get('/logout', (req, res, next) => {
     err.status = 403;
     next(err);
   }
-})
+});
 
 module.exports = router;
